37f0873093eb55a30aed654ad4638a6006427330
vshantam <vshantam@yahoo.com>
2018-06-12 23:48:12 +0530

    updated

--- train.py
+++ train.py
@@ -4,7 +4,10 @@ import h5py
 from sklearn.model_selection import train_test_split
 from keras.models import load_model
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, LeakyReLU, BatchNormalization
+from keras.layers import Dense, Dropout
+from keras import optimizers
+from sklearn.utils import shuffle
+import matplotlib.pyplot as plt
 
 
 class Train(object):
@@ -19,8 +22,8 @@ class Train(object):
 	#shuffeling the data
 	@classmethod
 	def shuffle(self):
-		self.x = self.x.sample(frac=1).reset_index(drop=True)
-		self.y = self.y.sample(frac=1).reset_index(drop=True)
+		self.x = shuffle(self.x, random_state=0)
+		self.y = shuffle(self.y, random_state=0)
 		return self.x, self.y
 
 	#split train and test data
@@ -29,51 +32,12 @@ class Train(object):
 		self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(self.x,self.y, test_size=0.33, random_state=42)
 		return self.x_train, self.x_test, self.y_train, self.y_test
 
-	#creating model
-	@classmethod
-	def create_model(self):
-		self.model = Sequential()
-
-		self.model.add(Dense(64, input_dim=12, init='uniform'))
-		self.model.add(LeakyReLU(alpha=0.3))
-		self.model.add(BatchNormalization(epsilon=1e-06, mode=0, momentum=0.9, weights=None))
-		self.model.add(Dropout(0.5)) 
-
-		self.model.add(Dense(64, init='uniform'))
-		self.model.add(LeakyReLU(alpha=0.3))
-		self.model.add(BatchNormalization(epsilon=1e-06, mode=0, momentum=0.9, weights=None))
-		self.model.add(Dropout(0.5))
-
-
-		self.model.add(Dense(64, init='uniform'))
-		self.model.add(LeakyReLU(alpha=0.3))
-		self.model.add(BatchNormalization(epsilon=1e-06, mode=0, momentum=0.9, weights=None))
-		self.model.add(Dropout(0.5))
-
-		self.model.add(Dense(64, init='uniform'))
-		self.model.add(LeakyReLU(alpha=0.3))
-		self.model.add(BatchNormalization(epsilon=1e-06, mode=0, momentum=0.9, weights=None))
-		self.model.add(Dropout(0.5))
-
-
-		self.model.add(Dense(1, init='uniform'))
-		self.model.add(Activation('softmax'))
-		return self.model
-
-	#training
-	@classmethod
-	def train(self):
-		self.model = create_model()
-		sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
-		self.model.compile(loss='binary_crossentropy', optimizer=sgd)
-		self.model.fit(self.x_train, self.y_train, nb_epoch=20, batch_size=16, show_accuracy=True, validation_split=0.2, verbose=2)
-		return self.model
 
 
 	#saving trained model
 	@classmethod
-	def save_trained_model(self):
-		self.model.save_weights("classifier/clf.h5py")
+	def save_trained_model(self, model):
+		model.save_weights("classifier/clf.h5py")
 
 if __name__ == "__main__":
 
@@ -81,7 +45,31 @@ if __name__ == "__main__":
 	x, y = obj.load("PreProcessed_Data/input.csv", "PreProcessed_Data/output.csv")
 	x, y = obj.shuffle()
 	x_train, x_test, y_train, y_test = obj.split_data()
-	print(x_train)
 
+	# Initialising the ANN
+	classifier = Sequential()
+	
+	# Adding the input layer and the first hidden layer
+	classifier.add(Dense(output_dim = 10, init = 'uniform', activation = 'relu', input_dim = 12))
+	
+	# Adding the second hidden layer
+	classifier.add(Dense(output_dim = 10, init = 'uniform', activation = 'relu'))
+	
+	# Adding the third hidden layer
+	classifier.add(Dense(output_dim = 10, init = 'uniform', activation = 'relu'))
+	
+	# Adding the output layer
+	classifier.add(Dense(output_dim = 1, init = 'uniform', activation = 'sigmoid'))
+	
+	# Compiling the ANN
+	classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+	
+	# Fitting the ANN to the Training set
+	history = classifier.fit(x_train, y_train, batch_size = 20, nb_epoch = 200)
+	
+	obj.save_trained_model(classifier)
 
+	plt.plot(history.history['loss'], history.history['acc'])
+
+	plt.show()
 
