dc0ebcbfebaa8019a8688351c8b00ba816a5f49b
vshantam <vshantam@yahoo.com>
2018-07-01 19:31:18 +0530

    update main.py

--- main.py
+++ main.py
@@ -24,12 +24,22 @@ if __name__ == '__main__':
 	print("Here are the list of extracted fetures from the audio files available in the database!")
 	dataset = pd.read_csv("Datasets/voicegender(kaggle-extracted-dataset)/voice.csv")
 	print(dataset.iloc[:,:-1].values)
-	print("\n\nThere are list of extracted values from ranges 0 - 3167 audio files consist of 12 fetures")
-	print("\n\nPlease select from the above extracted data usng serial number as 0 - 3167 \n")
-	resp = int(input("Please enter the number:"))
-	print(dataset.iloc[:,:-1].values[resp])
-	classifier = load_model("classifier/clf.h5py")
-	print (classifier.predict(dataset.iloc[:,:-1].values[resp].reshape(-1,1).T))
+
+	while True:
+		print("\n\nThere are list of extracted values from ranges 0 - 3167 audio files consist of 12 fetures")
+		print("\n\nPlease select from the above extracted data usng serial number as 0 - 3167 \n")
+		resp = int(input("Please enter the number:"))
+		print(dataset.iloc[:,:-1].values[resp])
+		classifier = load_model("classifier/clf.h5py")
+		res = (classifier.predict(dataset.iloc[:,:-1].values[resp].reshape(-1,1).T))
+		print(res)
+		if res >= .22 :
+			print("Predicted Gender is : {}".format("Female"))
+		else:
+			print("Predicted Gender is : Male")
+	
+		print("The actual gender is : {}".format(dataset.iloc[:, -1].values[resp]))
+
 	
 
 
