edc6a17c0711c304a1f660fb8e04729bb8023bf6
vshantam <vshantam@yahoo.com>
2018-07-16 23:26:41 +0530

    updated

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import numpy as np
 import sys,os
 from colorama import init
 from termcolor import cprint
+import termcolor
 from pyfiglet import figlet_format
 import keras
 import h5py
@@ -16,24 +17,33 @@ from keras.models import load_model
 from keras.layers import Dense
 from sklearn.preprocessing import StandardScaler
 
+def prCyan(skk):
+	print("\033[96m {}\033[00m" .format(skk))
+
+def prRed(skk):
+	print("\033[91m {}\033[00m" .format(skk))
 
 if __name__ == '__main__':
+
+	os.system("reset")
 
 	print(__doc__)
 
 	#Ascii art 
 	init(strip=not sys.stdout.isatty())
-	cprint(figlet_format('G D S!', font='starwars'), 'green', 'on_red', attrs=['bold'])
+	cprint(figlet_format('G D S!', font='starwars'), 'blue', 'on_red', attrs=['bold'])
 	print("Gender Detection System !")
 	print()
-	print("Here are the list of extracted fetures from the audio files available in the database!")
+	print(termcolor.colored("Here are the list of extracted features from the audio files available in the database!\n\n","red","on_white"))
 	dataset = pd.read_csv("Datasets/voicegender(kaggle-extracted-dataset)/voice.csv")
 	print(dataset.iloc[:,:-1].values)
 
 	while True:
-		print("\n\nThere are list of extracted values from ranges 0 - 3167 audio files consist of 12 fetures")
+
+		prCyan("\n\nThere are list of extracted values from ranges 0 - 3167 audio files consist of 12 fetures")
 		print("\n\nPlease select from the above extracted data usng serial number as 0 - 3167 \n")
-		resp = int(input("Please enter the number:"))
+		prRed("Please enter the number:")
+		resp = int(input())
 		print(dataset.iloc[:,:-1].values[resp])
 		classifier = load_model("classifier/clf.h5py")
 		scaler = StandardScaler()
